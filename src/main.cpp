4fbe313a84820e49390ae58e20c87b02463aeec8
Dominik Berner <dominik.berner@gmail.com>
2023-06-05 15:05:54 +0200

    Trivial Solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,73 @@
 #include <array>
+#include <chrono>
+#include <cstdio>
+#include <curses.h>
 #include <functional>
 #include <ncurses.h>
 #include <random>
 #include <signal.h>
+#include <thread>
 
 volatile sig_atomic_t stop;
 
 void inthand(int signum) { stop = 1; }
-constexpr int MAX_X = 100;
-constexpr int MAX_Y = 100;
+constexpr int MAX_X = 30;
+constexpr int MAX_Y = 30;
 WINDOW *win;
+
+unsigned int
+countNeighbours(const std::array<std::array<bool, MAX_X>, MAX_Y> &state, int i,
+                int j) {
+  unsigned int livingNeighbors = 0;
+  if (i > 0) {
+    if (j > 0) {
+      livingNeighbors += state[i - 1][j - 1] ? 1 : 0;
+    }
+    livingNeighbors += state[i - 1][j];
+    if (j < MAX_X - 1) {
+      livingNeighbors += state[i - 1][j + 1] ? 1 : 0;
+    }
+  }
+  if (j > 0) {
+    livingNeighbors += state[i][j - 1];
+  }
+  if (j < MAX_X - 1) {
+    livingNeighbors += state[i][j + 1];
+  }
+  if (i < MAX_Y - 1) {
+    if (j > 0) {
+      livingNeighbors += state[i + 1][j - 1];
+    }
+    livingNeighbors += state[i + 1][j];
+    if (j < MAX_X - 1) {
+      livingNeighbors += state[i + 1][j + 1];
+    }
+  }
+  return livingNeighbors;
+}
+
+std::array<std::array<bool, MAX_X>, MAX_Y>
+updateState(const std::array<std::array<bool, MAX_X>, MAX_Y> &state) {
+  auto newState = state;
+
+  for (int i = 0; i < MAX_Y; ++i) {
+    for (int j = 0; j < MAX_X; ++j) {
+      // count living neighbors
+      unsigned int livingNeighbors = countNeighbours(state, i, j);
+
+      if (state[i][j]) {
+        if (livingNeighbors < 2 || livingNeighbors > 3) {
+          newState[i][j] = false;
+        } else if (livingNeighbors == 3) {
+          newState[i][j] = true;
+        }
+      } else if (!state[i][j] && livingNeighbors == 3) {
+        newState[i][j] = true;
+      }
+    }
+  }
+  return newState;
+}
 
 void printState(const std::array<std::array<bool, MAX_X>, MAX_Y> &state) {
   // swap buffer with actual window
@@ -43,19 +101,25 @@ void seedState(std::array<std::array<bool, MAX_X>, MAX_Y> &state) {
   std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<> distrib(0, 1);
 
-  unsigned int x = 0;
-  for (auto &row : state) {
+  // unsigned int x = 0;
+  // for (auto &row : state) {
 
-    for (auto &cell : row) {
-      cell = distrib(gen);
-      ++x;
-    }
-  }
+  //   for (auto &cell : row) {
+  //     cell = distrib(gen);
+  //     ++x;
+  //   }
+  // }
+  // create a glider
+  state[0][2] = true;
+  state[1][0] = true;
+  state[1][2] = true;
+  state[2][1] = true;
+  state[2][2] = true;
 }
 
 int main(int, char **) {
 
-  std::array<std::array<bool, MAX_X>, MAX_Y> state;
+  std::array<std::array<bool, MAX_X>, MAX_Y> state{false};
 
   // register CTRL-C handler
   signal(SIGINT, inthand);
@@ -68,10 +132,12 @@ int main(int, char **) {
     // Add your implementation here
 
     printState(state);
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    // getch();
+    state = updateState(state);
   }
   // mvaddch(h- 1, w-1, 'A');
 
-  // reset window
   endwin();
   return 0;
 }
